1472689151dde7516523a997fadcd290e21f090f
NEH <erik@bodigiti.com>
2020-06-12 08:19:32 -0400

    Unit Testing: Added test 'Test' and ran 'npm test' script from parent project and Jest ran and the test passed as expected.

---
+++ index.test.js
@@ -0,0 +1,23 @@
+// PASSED TEST: with nothing but following 3 lines, validated jest passing test
+test("should be 1", () => {
+  expect(1).toBe(1);
+});
+
+// Test with Jest
+//const main = require("./index");
+
+
+
+/*
+test("Test something", () => {
+  expect(main.schemaApp('app', 'schemaApp')).toBe();
+});
+*/
+
+/*
+
+test("Test something", () => {
+  expect(testFileParens.file("./code.lisp.txt")).toBe(true);
+});
+
+*/
