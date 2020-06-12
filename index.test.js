a52eb98f3d6f46d200728e4e5cb9fc6c06e5bc12
NEH <erik@bodigiti.com>
2020-06-12 09:07:33 -0400

    Unit Testing: Add made-up test-test again and it still works.

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 // PASSED TEST: with nothing but following 3 lines, validated jest passing test
-test("should be 1", () => {
-  expect(1).toBe(1);
-});
+//test("should be 1", () => {
+  //expect(1).toBe(1);
+//});
 
 // Test with Jest
 //const main = require("./index");
@@ -13,6 +13,15 @@ process.env.GCLOUD_PROJECT = projectId;
 process.env.FIRESTORE_EMULATOR_HOST = "localhost:8000";
 let app = admin.initializeApp({projectId})
 let db = firebase.firestore(app)
+
+// clear all db data before each testing procedure [ doing this breaks testing ]
+//beforeAll(async () => {
+  //await firebase.clearFirestoreData({projectId});
+//})
+
+test("should be 1", () => {
+  expect(1).toBe(1);
+});
 
 
 /*
