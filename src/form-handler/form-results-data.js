3fa5404e0aace1f3fbabba4b4c16ce35b81e0018
NEH <erik@bodigiti.com>
2020-06-30 12:47:32 -0400

    Form Handler Multiple Files + Akismet: moved Akismet handling to file spam-check-akismet.js

--- src/form-handler/form-results-data.js
+++ src/form-handler/form-results-data.js
@@ -4,10 +4,12 @@
 ------------------------------------------------------------------------------*/
 
 /*-- Dependencies ------------------------------------------------------------*/
-const { AkismetClient } = require('akismet-api/lib/akismet.js'); // had to hardcode path
 const path = require('path');
 const { sortObjectsAsc, objectValuesByKey } =
   require(path.join(__dirname, "../utility"));
+
+const spamCheckAkismet = require('./spam-check-akismet');
+
 
 const formResultsData = async (req, admin, db, formSubmission, app, globalApp) => {
 
@@ -146,98 +148,14 @@ const formResultsData = async (req, admin, db, formSubmission, app, globalApp) =
     })();
     // Set allowed props
     props.set(propsAllowedEntries);
+
+    const propsForSpam = props.get().data;
+    console.log("propsForSpam $$$$$$$$$$$$$$$$$$$$$$$$$$$$$ ", propsForSpam);
+    const akismetResults = await spamCheckAkismet(req, formTemplateRef, propsForSpam, app, globalApp);
+    console.log("akismetResults $$$$$$$$$$$$$$$$$$$$$$$$$$$$ ", akismetResults);
+    props.set(akismetResults);
     //
     // [END] Props Set & Get
-    ////////////////////////////////////////////////////////////////////////////
-
-
-    ////////////////////////////////////////////////////////////////////////////
-    // Akismet Spam Filter
-    // If enabled:
-    //  1) Checks if spam
-    //     a. minimally checks IP Address and User Agent
-    //     b. checks fields defined as 'content' and 'other' based on config
-    //  2) Sets props
-    //     a. spam
-    //     b. toUidsSpamOverride (if spam, string overrides UID to prevent email)
-    //
-    let akismetEnabled = false;
-    if (globalApp.condition.spamFilterAkismet === 1
-        || (globalApp.condition.spamFilterAkismet === 2
-            && !!app.condition.spamFilterAkismet)
-    ) {
-      akismetEnabled = true;
-    }
-
-    if (akismetEnabled) {
-      // Akismet credentials
-      const key = app.spamFilterAkismet.key;
-      const blog = app.appInfo.appUrl;
-      const client = new AkismetClient({ key, blog })
-
-      try {
-        // Returns akismet props either as string or {}
-        // ternary with reduce
-        const akismetProps = fieldGroup => accumulatorType =>
-          // if database contains fieldsAkismet and [fieldGroup] array
-          ( typeof formTemplateRef.data().fieldsAkismet !== 'undefined'
-            && typeof formTemplateRef.data().fieldsAkismet[fieldGroup] !== 'undefined'
-            && formTemplateRef.data().fieldsAkismet[fieldGroup].length > 0)
-          // if true then reduce
-          ? (formTemplateRef.data().fieldsAkismet[fieldGroup].reduce((a, field) => {
-            // skip if field not found in props.get()...
-            if (typeof props.get().data.template.data[field] === 'undefined') {
-              return a
-            }
-            // accumulate as 'string' or {} based on accumulatorType
-            if (typeof accumulatorType === 'string') {
-              return a + props.get().data.template.data[field] + " ";
-            } else if (accumulatorType.constructor === Object) {
-              a[field] = props.get().data.template.data[field];
-              return a;
-            }
-          }, accumulatorType))
-          // if false then null
-          : null;
-
-          // Data to check for spam
-        const dataToCheck = {
-          ...req.ip && { ip: req.ip },
-          ...req.headers['user-agent'] && { useragent: req.headers['user-agent'] },
-          ...akismetProps('content')('') && { content: akismetProps('content')('') },
-          ...akismetProps('other')({})
-        }
-
-        // Test if data is spam: a successful test returns boolean
-        const isSpam = await client.checkSpam(dataToCheck);
-        // if spam suspected
-        if (typeof isSpam === 'boolean' && isSpam) {
-          props.set({spam: 'true' });
-          props.set({toUidsSpamOverride: "SPAM_SUSPECTED_DO_NOT_EMAIL" });
-        }
-        // if spam not suspected
-        else if (typeof isSpam === 'boolean' && !isSpam) {
-          props.set({spam: 'false' });
-        }
-
-      } catch(err) {
-
-        // Validate API Key
-        const isValid = await client.verifyKey();
-        if (isValid) {
-          console.info('Akismet: API key is valid');
-        } else if (!isValid) {
-          console.warn('Akismet: Invalid API key');
-        }
-
-        // if api key valid: error is likely network failure of client.checkSpam()
-        console.error("Akismet ", err);
-
-      }
-
-    }
-    //
-    // [END] Akismet Spam Filter
     ////////////////////////////////////////////////////////////////////////////
 
     return props.get().data
