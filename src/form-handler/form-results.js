abd495e4e60bb239f64683d92430e0b6dc8182ad
NEH <erik@bodigiti.com>
2020-07-01 02:10:18 -0400

    Form Handler + Form-Results: Refactor 'propsAllowed' from multiple un-grouped functions to get propsAllowed() from one async function with an await var.

--- src/form-handler/form-results.js
+++ src/form-handler/form-results.js
@@ -18,7 +18,7 @@ const formResults = async (req, admin, db, formSubmission, app, globalApp) => {
   //////////////////////////////////////////////////////////////////////////////
 
   // Consolidate props and fields last-in overwrite previous
-  const propsConsolidated = async () => {
+  const propsAll = async () => {
     // Form Field Defaults: select all default fields from database
     const formFieldsDefaultRef = await db.collection('formField')
       .where('default', '==', true).get();
@@ -32,11 +32,9 @@ const formResults = async (req, admin, db, formSubmission, app, globalApp) => {
       appKey: app.id, ...formFieldsDefault, ...formSubmission, ...app.appInfo
     }
   };
-  const propsAll = await propsConsolidated();
-//  console.log("propsAll $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ ", propsAll);
 
   //////////////////////////////////////////////////////////////////////////////
-  // Props Allowed Entries: reduce to allowed props
+  // Props Allowed: reduce entries to allowed props
   //////////////////////////////////////////////////////////////////////////////
 
   // Remove the fields not used for database or code actions to:
@@ -51,41 +49,50 @@ const formResults = async (req, admin, db, formSubmission, app, globalApp) => {
   //   formTemplate/'templateName'/fields --> formTemplateFields
   //   app/'appKey'/appInfo.*props --> appInfo
 
-  // Form Fields Required: fields required for cloud function to work
-  // Return Array of field names
-  const formFieldsRequiredRef = await db.collection('formField')
-    .where('required', '==', true).get();
+  const propsAllowed = async () => {
+    const allProps = await propsAll();
 
-  const formFieldsRequired = formFieldsRequiredRef.docs.reduce((a, doc) => {
-    a.push(doc.id);
-    return a;
-  }, []);
+    // Form Fields Required: fields required for cloud function to work
+    // Return Array of field names
+    const formFieldsRequiredRef = await db.collection('formField')
+      .where('required', '==', true).get();
 
-  // Form Template Fields Sorted:
-  // Array for submitForm/.../template.data used by 'trigger email' extension
-  const formTemplateRef = await db.collection('formTemplate')
-    .doc(propsAll.templateName).get();
-
-  const formTemplate = formTemplateRef.data();
-
-  const formTemplateFieldsSorted = objectValuesByKey(
-    sortObjectsAsc(formTemplate.fields, 'position'), 'id');
-
-  // Props Whitelist:
-  // Keys-allowed array for database & code actions last-in overwrite previous
-  const propsWhitelist = [ ...formFieldsRequired, ...formTemplateFieldsSorted,
-    ...Object.keys(app.appInfo)
-  ];
-
-  // Props Allowed Entries:
-  // Return Object entries used for database or code actions
-  const propsAllowedEntries = Object.entries(propsAll).reduce(
-    (a, [key, value]) => {
-      if (propsWhitelist.includes(key)) {
-        a[key] = value;
-      }
+    const formFieldsRequired = formFieldsRequiredRef.docs.reduce((a, doc) => {
+      a.push(doc.id);
       return a;
-    }, {});
+    }, []);
+
+    // Form Template Fields Sorted:
+    // Array for submitForm/.../template.data used by 'trigger email' extension
+    const formTemplateRef = await db.collection('formTemplate')
+      .doc(allProps.templateName).get();
+
+    const formTemplate = formTemplateRef.data();
+
+    const formTemplateFieldsId = objectValuesByKey(formTemplate.fields, 'id');
+
+    // Props Whitelist:
+    // Keys-allowed array for database & code actions last-in overwrite previous
+    const propsWhitelistKeys = [
+      ...formFieldsRequired,
+      ...formTemplateFieldsId,
+      ...Object.keys(app.appInfo)
+    ];
+
+    // Props Allowed Entries:
+    // Return Object entries used for database or code actions
+    const allowedEntries = Object.entries(allProps).reduce(
+      (a, [key, value]) => {
+        if (propsWhitelistKeys.includes(key)) {
+          a[key] = value;
+        }
+        return a;
+      }, {});
+
+    return { entries: allowedEntries, formTemplate, formTemplateFieldsId }
+    }
+  // Get Props Allowed
+  const allowedProps = await propsAllowed();
 
   //////////////////////////////////////////////////////////////////////////////
   // Props Set & Get
@@ -106,13 +113,12 @@ const formResults = async (req, admin, db, formSubmission, app, globalApp) => {
           props.toUids = value;
         } else if (key === 'spam') {
           // if spam then override toUids value so email is not sent
-          value === 'true' && (props.toUids = "SPAM_SUSPECTED_DO_NOT_EMAIL");
+          (value === 'true') && (props.toUids = "SPAM_SUSPECTED_DO_NOT_EMAIL");
         }
-        // Form Template Fields: Whitelist check [START]
-        if (formTemplateFieldsSorted.includes(key)) {
+        // Form Template Fields: Whitelist check
+        if (allowedProps.formTemplateFieldsId.includes(key)) {
           props.templateData[key] = value;
         }
-        // Form Template Fields: Whitelist check [END]
       });
 
     const getProps =
@@ -145,7 +151,7 @@ const formResults = async (req, admin, db, formSubmission, app, globalApp) => {
   //////////////////////////////////////////////////////////////////////////////
   // Set allowed props
   //////////////////////////////////////////////////////////////////////////////
-  props.set(propsAllowedEntries);
+  props.set(allowedProps.entries);
 
   //////////////////////////////////////////////////////////////////////////////
   // Spam Check Akismet
@@ -155,7 +161,7 @@ const formResults = async (req, admin, db, formSubmission, app, globalApp) => {
           && !!app.condition.spamFilterAkismet)
   ) {
     const propsForSpamCheck = props.get().data;
-    const isSpam = await spamCheckAkismet(req, formTemplate,
+    const isSpam = await spamCheckAkismet(req, allowedProps.formTemplate,
       propsForSpamCheck, app);
     // Add spam check results to props if spam check enabled
     props.set(isSpam);
