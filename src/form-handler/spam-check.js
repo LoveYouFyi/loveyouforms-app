d972c5a5fbaa3cba43bd9dd9bc05dab1cc7bb6c2
NEH <erik@bodigiti.com>
2020-07-07 00:10:29 -0400

    Form-Handler + Spam Check: Extract akismetProps from primary spamCheck function, this simplifies the dataToCheck conditional props of 'content' and 'other'.

--- src/form-handler/spam-check.js
+++ src/form-handler/spam-check.js
@@ -9,6 +9,37 @@
 const { AkismetClient } = require('akismet-api/lib/akismet.js'); // had to hardcode path
 
 /*------------------------------------------------------------------------------
+  Akismet Props:
+  Returns 'string' or object {} to accommodate Akismet required data format
+  FYI: Ternary with reduce
+------------------------------------------------------------------------------*/
+const akismetProps = (formTemplateData, propsData, fieldGroup,
+  accumulatorType) => {
+  // if database contains fieldsSpamCheck and [fieldGroup] array
+  return (
+    (typeof formTemplateData.fieldsSpamCheck !== 'undefined'
+    && typeof formTemplateData.fieldsSpamCheck[fieldGroup] !== 'undefined'
+    && formTemplateData.fieldsSpamCheck[fieldGroup].length > 0)
+      // if true then reduce
+      ? (formTemplateData.fieldsSpamCheck[fieldGroup].reduce((a, field) => {
+        // skip if field not found in propsForSpam...
+        if (typeof propsData.template.data[field] === 'undefined') {
+          return a
+        }
+        // accumulate as 'string' or {} based on accumulatorType
+        if (typeof accumulatorType === 'string') {
+          return a + propsData.template.data[field] + " ";
+        } else if (accumulatorType.constructor === Object) {
+          a[field] = propsData.template.data[field];
+          return a;
+        }
+      }, accumulatorType))
+      // if false then null
+      : null
+  )
+}
+
+/*------------------------------------------------------------------------------
   Spam Check:
   Returns one of {spam: 'Check disabled '} {spam: 'true'} {spam: 'false'}
 ------------------------------------------------------------------------------*/
@@ -27,39 +58,26 @@ const spamCheck = async (req, app, globalApp, formTemplateData, propsData) => {
   const blog = app.appInfo.appUrl;
   const client = new AkismetClient({ key, blog });
 
-  // Returns akismet props either as string or {}
-  // ternary with reduce
-  const akismetProps = (fieldGroup, accumulatorType) =>
-    // if database contains fieldsSpamCheck and [fieldGroup] array
-    (typeof formTemplateData.fieldsSpamCheck !== 'undefined'
-      && typeof formTemplateData.fieldsSpamCheck[fieldGroup] !== 'undefined'
-      && formTemplateData.fieldsSpamCheck[fieldGroup].length > 0)
-    // if true then reduce
-    ? (formTemplateData.fieldsSpamCheck[fieldGroup].reduce((a, field) => {
-      // skip if field not found in propsForSpam...
-      if (typeof propsData.template.data[field] === 'undefined') {
-        return a
-      }
-      // accumulate as 'string' or {} based on accumulatorType
-      if (typeof accumulatorType === 'string') {
-        return a + propsData.template.data[field] + " ";
-      } else if (accumulatorType.constructor === Object) {
-        a[field] = propsData.template.data[field];
-        return a;
-      }
-    }, accumulatorType))
-    // if false then null
-    : null;
-
+  //////////////////////////////////////////////////////////////////////////////
   // Data to check for spam
   // IP Address: Akismet requires req.ip value so if undefined set to null;
-  // the conditional is needed because firebase emulator (dev environment)
-  // does not provide req.ip so we prevent unacceptable undefined IP property
+  //   the conditional is needed because firebase emulator (dev environment)
+  //   does not provide req.ip so we prevent unacceptable undefined IP property
+  //////////////////////////////////////////////////////////////////////////////
+  const dataContentString =
+    akismetProps(formTemplateData, propsData, 'content', '');
+  const dataOtherObject =
+    akismetProps(formTemplateData, propsData, 'other', {});
+
   const dataToCheck = {
+    // IP Address
     ip: (typeof req.ip !== 'undefined' ? req.ip : null),
+    // User Agent
     ...req.headers['user-agent'] && { useragent: req.headers['user-agent'] },
-    ...akismetProps('content', '') && { content: akismetProps('content', '') },
-    ...akismetProps('other', {})
+    // Content: expected as single string
+    ...dataContentString && { content: dataContentString },
+    // Other: expected as separate props
+    ...dataOtherObject
   }
 
   try {
@@ -67,9 +85,9 @@ const spamCheck = async (req, app, globalApp, formTemplateData, propsData) => {
     // Test if data is spam: a successful test returns boolean
     const isSpam = await client.checkSpam(dataToCheck);
     // return object since form-results props.set() expects {} entries
-    return typeof isSpam === 'boolean' ?
-      isSpam ? {spam: 'true'} : {spam: 'false'} :
-      "Check failed"
+    return (typeof isSpam === 'boolean')
+      ? (isSpam ? { spam: 'true' } : { spam: 'false' })
+      : "Check failed"
 
   } catch(err) {
 
