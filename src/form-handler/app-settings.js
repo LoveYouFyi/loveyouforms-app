4468c7f62cfe8357fded2608bb731b9b40b92044
NEH <erik@bodigiti.com>
2020-07-06 22:27:31 -0400

    Form-Handler + App-Settings: File renamed, and extracted & refactored functions that get app, globalApp, messages, and submitForm; also moved CORS Check to index.js.

--- src/form-handler/app-settings.js
+++ src/form-handler/app-settings.js
@@ -21,8 +21,7 @@ const getGlobalApp = async (db) => {
 }
 
 /*------------------------------------------------------------------------------
-  Messages:
-  Returns app-specific or globalApp messages based on config settings
+  Messages: app-specific or globalApp based on config settings
 ------------------------------------------------------------------------------*/
 const messagesAppVsGlobal = (app, globalApp) => {
   // global boolean 0/false, 1/true, or '2' bypass global & use app boolean
@@ -36,15 +35,32 @@ const messagesAppVsGlobal = (app, globalApp) => {
 }
 
 /*------------------------------------------------------------------------------
-  App Validate:
-  Returns object { app, globalApp, messages }
-  Check if cors authorized app, and form submit enabled
-  Stop processing if checks fail
+  Submit Form Enabled:
+  global boolean 0/false, 1/true, or '2' bypass global to use app boolean
 ------------------------------------------------------------------------------*/
-const appValidate = async (req, res, db, formSubmission) => {
+const submitFormEnabled = (app, globalApp) => {
+  // If disabled
+  if (globalApp.condition.submitForm === 0
+      || (globalApp.condition.submitForm === 2
+          && !app.condition.submitForm)
+  ) {
+    console.warn(`Form submit disabled for app "${app.appInfo.appName}"`);
+    return false;
+  } else {
+    return true;
+  }
+}
+
+/*------------------------------------------------------------------------------
+  App Validate:
+  If app does not exist return false... Else return app settings object { }
+  Check if cors authorized app, and form submit enabled
+------------------------------------------------------------------------------*/
+const appSettings = async (req, res, db, formSubmission) => {
+
   // App & App Check
   const app = await getApp(db, formSubmission);
-  // If app does not exist then stop processing
+  // If app key does not exist...
   if (!app) {
     console.warn('App Key does not exist.');
     // no error message sent to client because submit not from approved app
@@ -54,50 +70,20 @@ const appValidate = async (req, res, db, formSubmission) => {
   const globalApp = await getGlobalApp(db);
   const messages = messagesAppVsGlobal(app, globalApp);
 
-  //////////////////////////////////////////////////////////////////////////////
-  // CORS Validation
-  // Stop cloud function if check does not pass
-  // Global boolean 0/false, 1/true, or '2' bypass global to use app boolean
-  //////////////////////////////////////////////////////////////////////////////
-  if (globalApp.condition.corsBypass === 0
-      || (globalApp.condition.corsBypass === 2
-          && !app.condition.corsBypass)
-    ) {
-    // url requests restricted to match the app
-    res.setHeader('Access-Control-Allow-Origin', app.appInfo.appUrl);
-    // end processing if app url does not match req.headers.origin url
-    if (req.headers.origin !== app.appInfo.appUrl) {
-      console.warn('CORS Access Control: Origin Url does not match App Url.');
-      // no error response sent because request not from approved app
-      return res.end();
-    }
-  } else {
-    // allow all so localhost (or any source) can submit requests
-    res.setHeader('Access-Control-Allow-Origin', '*');
-  }
+
+
+  const submitForm = submitFormEnabled(app, globalApp);
 
   //////////////////////////////////////////////////////////////////////////////
-  // Form Submit Enabled: throw error if submitForm disabled
-  // global boolean 0/false, 1/true, or '2' bypass global to use app boolean
-  //////////////////////////////////////////////////////////////////////////////
-  if (globalApp.condition.submitForm === 0
-      || (globalApp.condition.submitForm === 2
-          && !app.condition.submitForm)
-    ) {
-    console.warn(`Form submit disabled for app "${app.appInfo.appName}"`);
-    // return error response because submit is from approved app
-    throw (messages.error.text);
-  }
-
-  //////////////////////////////////////////////////////////////////////////////
-  // Return
+  // If above returns were not triggered then return object
   //////////////////////////////////////////////////////////////////////////////
   return ({
     app,
     globalApp,
-    messages
-  })
+    messages,
+    submitForm
+  });
 
 }
 
-module.exports = appValidate;
+module.exports = appSettings;
--- src/form-handler/index.js
+++ src/form-handler/index.js
@@ -6,15 +6,14 @@
 
 /*-- Dependencies ------------------------------------------------------------*/
 const { logErrorInfo } = require("./../utility");
-const appValidate = require('./app-validate');
-const formResults = require('./form-results');
+const getAppSettings = require('./app-settings');
+const getFormResults = require('./form-results');
 
 /*------------------------------------------------------------------------------
   Export Form Handler Function
 ------------------------------------------------------------------------------*/
 module.exports = ({ admin }) => async (req, res) => {
   const db = admin.firestore();
-  let messages; // declared here so catch has access to config messages
   // Stop processing if content-type is 'undefined' or not 'text/plain'
   if (typeof req.headers['content-type'] === 'undefined'
     || req.headers['content-type'].toLowerCase() !== 'text/plain') {
@@ -25,33 +24,71 @@ module.exports = ({ admin }) => async (req, res) => {
   // Form submission string as object, req.body should be json formatted string
   const formSubmission = JSON.parse(req.body);
 
-  try {
+  const messages = {}; // declared here so catch has access to config messages
 
-    const validRequest = await appValidate(req, res, db, formSubmission);
-    // Stop processing if not valid request
-    if (!validRequest) {
+  try {
+    // Settings returns {} props
+    const appSettings = await getAppSettings(req, res, db, formSubmission);
+    // If app does not exist stop processing
+    if (!appSettings) {
       return res.end();
     }
-    const app = validRequest.app;
-    const globalApp = validRequest.globalApp; // declared here for akismet
-    messages = validRequest.messages; // either 'app' or 'globalApp' messages
+    // Set messages (taken from 'app' or 'globalApp' based on config)
+    Object.assign(messages, appSettings.messages);
+//    messages = appSettings.messages;
+
+    //////////////////////////////////////////////////////////////////////////////
+    // CORS Validation
+    // Stop cloud function if check does not pass
+    // Global boolean 0/false, 1/true, or '2' bypass global to use app boolean
+    //////////////////////////////////////////////////////////////////////////////
+    if (appSettings.globalApp.condition.corsBypass === 0
+        || (appSettings.globalApp.condition.corsBypass === 2
+            && !appSettings.app.condition.corsBypass)
+      ) {
+      // url requests restricted to match the app
+      res.setHeader('Access-Control-Allow-Origin', appSettings.app.appInfo.appUrl);
+      // end processing if app url does not match req.headers.origin url
+      if (req.headers.origin !== app.appInfo.appUrl) {
+        console.warn('CORS Access Control: Origin Url does not match App Url.');
+        // no error response sent because request not from approved app
+        return res.end();
+      }
+    } else {
+      // allow all so localhost (or any source) can submit requests
+      res.setHeader('Access-Control-Allow-Origin', '*');
+    }
+
+
+    // If submit form disabled
+    if (!appSettings.submitForm) {
+      console.log("error $$$$$$$$$$$$$$$$$$$$$$$$$$$ ", messages.error);
+      return res.status(200).send({
+        data: {
+          message: messages.error
+        }
+      });
+    }
 
     ////////////////////////////////////////////////////////////////////////////
     // Database Entry: add form submission to database
     ////////////////////////////////////////////////////////////////////////////
-    const formHandlerResults = await formResults(req, admin, db, formSubmission,
-      app, globalApp);
+    const formResults = await getFormResults(req, admin, db, formSubmission,
+      appSettings.app, appSettings.globalApp);
     // For serverTimestamp to work must first create new doc key then 'set' data
     const newKeyRef = db.collection('submitForm').doc();
     // update the new-key-record using 'set' which works for existing doc
-    newKeyRef.set(formHandlerResults.data);
+    newKeyRef.set(formResults.data);
 
     ////////////////////////////////////////////////////////////////////////////
     // Response: return object (even if empty) so client can finish AJAX success
     ////////////////////////////////////////////////////////////////////////////
+    console.log("formResults $$$$$$$$$$$$$$$$$$$$$$$$$$$ ", formResults.urlRedirect);
+    console.log("success $$$$$$$$$$$$$$$$$$$$$$$$$$$ ", messages.success);
+
     return res.status(200).send({
       data: {
-        redirect: formHandlerResults.urlRedirect,
+        redirect: formResults.urlRedirect,
         message: messages.success
       }
     });
