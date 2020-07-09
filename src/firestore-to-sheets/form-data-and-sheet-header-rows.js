0b41baded6639f16ef47bfc7e23acddffacfabfd
NEH <erik@bodigiti.com>
2020-07-09 14:47:13 -0400

    Firestore To Sheets + Split MultiFile + Form-Data-And-Sheet-Header-Rows: Removed the 'try/catch' and moved getFormTemplate out of primary function.

--- src/firestore-to-sheets/form-data-and-sheet-header-rows.js
+++ src/firestore-to-sheets/form-data-and-sheet-header-rows.js
@@ -8,6 +8,16 @@ const { logErrorInfo, sortObjectsAsc, objectValuesByKey } =
   require("../utility");
 
 /*------------------------------------------------------------------------------
+  Form Template: For Template Field Ids and Header Row Sheet Columns.
+  Database needs to have Fields Ids and Header Columns sorted to match
+  templateData array is sorted to match the order of headerRowSheet
+------------------------------------------------------------------------------*/
+const getFormTemplate = async (db, templateName) => {
+ const gotFormTemplate = await db.collection('formTemplate').doc(templateName).get();
+  return gotFormTemplate.data();
+}
+
+/*------------------------------------------------------------------------------
   Form Data and Sheet Header Rows:
   ...
 ------------------------------------------------------------------------------*/
@@ -17,76 +27,64 @@ const formDataAndSheetHeaderRows = async (snapshot, db, app) => {
   const { appKey, createdDateTime, template: { data: { ...templateData },
     name: templateName  } } = snapshot.data();
 
-  try {
+  const formTemplate = await getFormTemplate(db, templateName);
 
-    ////////////////////////////////////////////////////////////////////////////
-    // Prepare data row values and sheet header
-    ////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  // Form Data Row:
+  // Sort and merge data row to be sent to sheets
+  //////////////////////////////////////////////////////////////////////////////
 
-    // Form Template for Template Field Ids and Header Row Sheet Columns
-    // Database needs to have Fields Ids and Header Columns sorted to match
-    // templateData array is sorted to match the order of headerRowSheet
-    const formTemplateRef = await db.collection('formTemplate').doc(templateName).get();
-    const formTemplate = formTemplateRef.data();
+  // Fields Ids Sorted: required for sorting templateData so data row that is sent
+  // to sheets will be sorted in the same order as the sheet's column header
+  const formTemplateFieldsIdsSorted = objectValuesByKey(
+    sortObjectsAsc(formTemplate.fields, "position"), "id");
 
-    // Fields Ids Sorted: required for sorting templateData so data row that is sent
-    // to sheets will be sorted in the same order as the sheet's column header
-    const formTemplateFieldsIdsSorted = objectValuesByKey(
-      sortObjectsAsc(formTemplate.fields, "position"), "id");
+  // timezone 'tz' string defined by momentjs.com/timezone:
+  // https://github.com/moment/moment-timezone/blob/develop/data/packed/latest.json
+  const dateTime = createdDateTime.toDate(); // toDate() is firebase method
+  const createdDate = moment(dateTime).tz(app.appInfo.appTimeZone).format('L');
+  const createdTime = moment(dateTime).tz(app.appInfo.appTimeZone).format('h:mm A z');
 
-    // Fields Sheet Headers Sorted: required for spreadsheet column headers when
-    // adding a new sheet to a spreadsheet
-    // Sheets requires a nested array of strings [ [ 'Date', 'Time', etc ] ]
-    const sheetHeaderRowSorted = [
-      [
-        'Date', 'Time',
-        ...objectValuesByKey(
-          sortObjectsAsc(formTemplate.fields, "position"), "sheetHeader")
-      ]
-    ];
+  // Template Data Sorted: returns an object that contains the new
+  // formSubmit record's data sort-ordered to match formTemplate fields positions
+  const templateDataSorted = formTemplateFieldsIdsSorted.reduce((a, fieldName) => {
+    // if fieldName data not exist set empty string since config sort order requires it
+    templateData[fieldName] ? a[fieldName] = templateData[fieldName] : a[fieldName] = "";
+    return a
+  }, {});
 
-    ////////////////////////////////////////////////////////////////////////////
-    // Row Data: Sort and Merge (data row to be sent to sheets)
-    //
-
-    // timezone 'tz' string defined by momentjs.com/timezone:
-    // https://github.com/moment/moment-timezone/blob/develop/data/packed/latest.json
-    const dateTime = createdDateTime.toDate(); // toDate() is firebase method
-    const createdDate = moment(dateTime).tz(app.appInfo.appTimeZone).format('L');
-    const createdTime = moment(dateTime).tz(app.appInfo.appTimeZone).format('h:mm A z');
-
-    // Template Data Sorted: returns an object that contains the new
-    // formSubmit record's data sort-ordered to match formTemplate fields positions
-    const templateDataSorted = formTemplateFieldsIdsSorted.reduce((a, fieldName) => {
-      // if fieldName data not exist set empty string since config sort order requires it
-      templateData[fieldName] ? a[fieldName] = templateData[fieldName] : a[fieldName] = "";
-      return a
-    }, {});
-
-    // Merge objects in sort-order and return only values
-    // Data-row for sheet requires nested array of strings [ [ 'John Smith', etc ] ]
-    const formDataRowSorted = [(
-      Object.values({
-        createdDate,
-        createdTime,
-        ...templateDataSorted
-      })
-    )];
-    //
-    // [END] Row Data: Sort and Merge
-    ////////////////////////////////////////////////////////////////////////////
-
-    return ({
-      sheetHeaderRowSorted,
-      formDataRowSorted
+  // Merge objects in sort-order and return only values
+  // Data-row for sheet requires nested array of strings [ [ 'John Smith', etc ] ]
+  const formDataRowSorted = [(
+    Object.values({
+      createdDate,
+      createdTime,
+      ...templateDataSorted
     })
+  )];
+  //
+  // [END] Row Data: Sort and Merge
+  //////////////////////////////////////////////////////////////////////////////
 
 
-  } catch(error) {
+  //////////////////////////////////////////////////////////////////////////////
+  // Sheet Header Row Sorted: required for spreadsheet column headers when
+  // adding a new sheet to a spreadsheet
+  // Sheets requires a nested array of strings [ [ 'Date', 'Time', etc ] ]
+  //////////////////////////////////////////////////////////////////////////////
+  const sheetHeaderRowSorted = [
+    [
+      'Date', 'Time',
+      ...objectValuesByKey(
+        sortObjectsAsc(formTemplate.fields, "position"), "sheetHeader")
+    ]
+  ];
 
-    console.error(logErrorInfo(error));
 
-  }
+  return ({
+    sheetHeaderRowSorted,
+    formDataRowSorted
+  })
 
 }
 
