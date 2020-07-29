2c161619255a2045fd4fafbb0941361f7d75666f
NEH <erik@bodigiti.com>
2020-07-29 15:58:09 -0400

    LYF-Package/index.js: Simply edited formatting and positioning of code-lines

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
 /*------------------------------------------------------------------------------
-  Dependencies all cloud functions
+  Cloud Functions
 ------------------------------------------------------------------------------*/
 
+/*-- Dependencies ------------------------------------------------------------*/
 // Firebase Functions SDK: to create Cloud Functions and setup triggers
 const functions = require('firebase-functions');
 // Environment Keys: to set variables based on environments dev vs prod
 const env = require('./../../env/keys.public.js');
 
-/*------------------------------------------------------------------------------
-  Cloud Functions
-------------------------------------------------------------------------------*/
-const formHandler = require('./src/form-handler');
-const firestoreToSheets = require('./src/firestore-to-sheets');
-const schemaDefault = require('./src/schema-default');
-
 
 /*-- Form-Handler HTTP Cloud Function ----------------------------------------*/
-module.exports.formHandler = functions.https.onRequest(formHandler(env));
+const formHandler = require('./src/form-handler');
+module.exports.formHandler = functions.https
+  .onRequest(formHandler(env));
 
 
 /*-- Firestore-to-Sheets Trigger Cloud Function ------------------------------*/
-module.exports.firestoreToSheets = functions.firestore.document('submitForm/{formId}')
+const firestoreToSheets = require('./src/firestore-to-sheets');
+module.exports.firestoreToSheets = functions.firestore
+  .document('submitForm/{formId}')
   .onCreate(firestoreToSheets(env));
 
 
 /*-- Schema-Default Trigger Cloud Functions ----------------------------------*/
+const schemaDefault = require('./src/schema-default');
 
 // 'app' collection default schema function
-module.exports.schemaApp = functions.firestore.document('app/{id}')
+module.exports.schemaApp = functions.firestore
+  .document('app/{id}')
   .onCreate(schemaDefault('app', 'schemaApp', env));
 
 // 'formTemplate' collection default schema function
-module.exports.schemaFormTemplate = functions.firestore.document('formTemplate/{id}')
+module.exports.schemaFormTemplate = functions.firestore
+  .document('formTemplate/{id}')
   .onCreate(schemaDefault('formTemplate', 'schemaFormTemplate', env));
